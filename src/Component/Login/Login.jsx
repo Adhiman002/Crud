2d84b60edd06e85d32738ec18bb5b4bd2ed2e83f
Abhishek Kumar <129261679+Adhiman002@users.noreply.github.com>
2024-03-02 22:56:12 +0530

    login and signup break in components

--- src/Component/Login/Login.jsx
+++ src/Component/Login/Login.jsx
@@ -4,10 +4,12 @@ import './Login.css'
 // import { faUser } from '@fortawesome/free-solid-svg-icons';
 import logo from '../../biology.jpg'
 import '@fortawesome/fontawesome-free/css/all.css'; // Import FontAwesome CSS
+import SignUp from './SignUpForm';
+import LoginForm from './LoginForm';
 
 function Login() {
   return (
-    <div>
+    <>
 
       <div className="container">
         <input type="checkbox" id="flip" />
@@ -29,54 +31,13 @@ function Login() {
         </div>
         <div className="forms">
           <div className="form-content">
-            <div className="login-form">
-              <div className="title">Login</div>
-              <form action="#">
-                <div className="input-boxes">
-                  <div className="input-box">
-                    <i className="fas fa-envelope"></i>
-                    <input type="text" placeholder="Enter your email" required />
-                  </div>
-                  <div className="input-box">
-                    <i className="fas fa-lock"></i>
-                    <input type="password" placeholder="Enter your password" required />
-                  </div>
-                  <div className="text"><a href="#">Forgot password?</a></div>
-                  <div className="button input-box">
-                    <input type="submit" value="Submit" />
-                  </div>
-                  <div className="text sign-up-text">Don't have an account? <label for="flip">Sigup now</label></div>
-                </div>
-              </form>
-            </div>
-            <div className="signup-form">
-              <div className="title">Signup</div>
-              <form action="#">
-                <div className="input-boxes">
-                  <div className="input-box">
-                    <i className="fas fa-user"></i>
-                    <input type="text" placeholder="Enter your name" required />
-                  </div>
-                  <div className="input-box">
-                    <i className="fas fa-envelope"></i>
-                    <input type="text" placeholder="Enter your email" required />
-                  </div>
-                  <div className="input-box">
-                    <i className="fas fa-lock"></i>
-                    <input type="password" placeholder="Enter your password" required />
-                  </div>
-                  <div className="button input-box">
-                    <input type="submit" value="Submit" />
-                  </div>
-                  <div className="text sign-up-text">Already have an account? <label for="flip">Login now</label></div>
-                </div>
-              </form>
-            </div>
+
+            <LoginForm />
+            <SignUp />
           </div>
         </div>
       </div>
-
-    </div>
+    </>
   )
 }
 
---
+++ src/Component/Login/LoginForm.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const LoginForm = () => {
+    return (
+
+        <div className="login-form">
+            <div className="title">Login</div>
+            <form action="#">
+                <div className="input-boxes">
+                    <div className="input-box">
+                        <i className="fas fa-envelope"></i>
+                        <input type="text" placeholder="Enter your email" required />
+                    </div>
+                    <div className="input-box">
+                        <i className="fas fa-lock"></i>
+                        <input type="password" placeholder="Enter your password" required />
+                    </div>
+                    <div className="text"><a href="#">Forgot password?</a></div>
+                    <div className="button input-box">
+                        <input type="submit" value="Submit" />
+                    </div>
+                    <div className="text sign-up-text">Don't have an account? <label for="flip">Sigup now</label></div>
+                </div>
+            </form>
+        </div>
+
+    )
+}
+
+export default LoginForm
